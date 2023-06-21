b5351adb9936fe64e22b664e7104c34750f27616
TSPabhishek <91623079+TSPabhishek@users.noreply.github.com>
2023-06-22 02:38:16 +0530

    Add files via upload

---
+++ src/Components/SelfIntro/SelfIntro.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { SiLichess } from "react-icons/si"
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function SelfIntro() {
+  return (
+    <Container fluid className="home-about-section  container-fluid" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I fell in love with programming and I have at least learnt
+              something, I think… 🤷‍♂️
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple"> Java , python and JavaScript . </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are  &nbsp;
+              <i>
+                <b className="purple">Backend dev  &nbsp;</b> and also passionate
+                about  &nbsp;<b className="purple">Chess</b>
+              </i>
+              <br />
+              <br />
+              Whenever possible, I also apply my passion for Playing chess and
+              solving <b className="purple">Tactics</b>
+              
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          {/* <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/TSPabhishek"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/tsp-abhishek/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/tsp_abhishek/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://lichess.org/@/tspabhshek"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <SiLichess />
+                </a>
+              </li>
+            </ul>
+          </Col> */}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default SelfIntro;
